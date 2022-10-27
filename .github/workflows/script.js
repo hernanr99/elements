ddc24d32c43cbc5f3ef5dc86da2b9d3c153cd5a4
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 16:42:30 -0300

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -10,11 +10,6 @@ module.exports = async ({core}, data) => {
   
   await core.summary
     .addHeading(`Reference`, 3)
-    .addTable([
-      [{ data: "Symbol", header: true }, { data: "Description", header: true }],
-      [":heavy_check_mark:", "Test passed"],
-      [":x:", "Test failed"],
-      [":no_entry_sign:", "Not tested"]
-    ])
+    .addTable(newStr)
     .write()
 }
