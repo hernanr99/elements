b6418e037b516d422a72b806ff7767e01226ecfb
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 18:36:37 -0300

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,14 +1,28 @@
 module.exports = async ({core}, data) => {  
   console.log(data);
-//   const algo = {
-//     results: '"[{\\"data\\":\\"Symbol\\",\\"header\\":true},{\\"data\\":\\"Description\\",\\"header\\":true}],[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]"'
-//   }
-//   const otro = algo.results.replaceAll('\\', '');
-//   const otro2 = otro.replaceAll('[', '');
-//   console.log(otro2);
+  console.log("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA);
+  console.log(data.VODSpeed);
+  console.log(data.Reference);
+  const algo = data.Reference;
+  const otro = algo.results.replaceAll('\\', '');
+  const otro3 = otro.replaceAll('"', '');
+  const otro2 = otro3.replaceAll('[', '');
+  console.log(otro2);
   
-//   const array = otro2.split("],");
-//   console.log(array);
+  const array = otro2.split("],");
+  for (i=0;i<array.length;i++) { 
+    array[i] = array[i].split(",")
+  }
+  console.log(array);
+
+  await core.summary
+    .addHeading(`Reference`, 3)
+    .addTable(array)
+    .write()
+}
+  
+  
+  
   await core.summary
     .addHeading(`Reference`, 3)
     .addTable([array])
