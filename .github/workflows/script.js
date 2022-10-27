85ecdc8e9e06b5f2c8d57918178252a6a23878b9
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 16:50:20 -0300

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,15 +1,16 @@
 module.exports = async ({core}, data) => {  
   console.log(data);
   const algo = {
-    results: '"[[{\\"data\\":\\"Symbol\\",\\"header\\":true},{\\"data\\":\\"Description\\",\\"header\\":true}],[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]]"'
+    results: '"[{\\"data\\":\\"Symbol\\",\\"header\\":true},{\\"data\\":\\"Description\\",\\"header\\":true}],[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]"'
   }
   const otro = algo.results.replaceAll('\\', '');
-  console.log(otro);
+  const otro2 = algo.results.replaceAll('[', '');
+  console.log(otro2);
   
-  const newStr = otro.substring(1, otro.length - 1);
-  
+  const array = otro.split("],");
+  console.log(array);
   await core.summary
     .addHeading(`Reference`, 3)
-    .addTable(newStr)
+    .addTable(array)
     .write()
 }
