662abf9d8265ccbe16fdff73bc60c5d3c52bb2ef
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 17:39:35 -0300

    Create hernan.js

---
+++ .github/workflows/hernan.js
@@ -0,0 +1,20 @@
+module.exports = async ({core}, data) => {  
+  console.log(data);
+  const algo = {
+    results: '"[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]"'
+  }
+  const otro = algo.results.replaceAll('\\', '');
+  const otro2 = otro.replaceAll('[', '');
+  console.log(otro2);
+  
+  const array = otro2.split("],");
+  for (let i of array.length) {
+    array[i] = array[i].split(",")
+  }
+  console.log(array);
+
+  await core.summary
+    .addHeading(`Reference`, 3)
+    .addTable([array])
+    .write()
+}
