a3857ae7cc68bdbe51082c9ccd5139074c28ed69
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 15:12:02 -0300

    Update script.js

--- .github/script.js
+++ .github/script.js
@@ -3,6 +3,8 @@ module.exports = async ({core}, data) => {
 
   await core.summary
     .addHeading("hola", 3)
-    .addTable([[{ data: "Test #", header: true }, { data: 'VIDEO_JS_7', header: true }, { data: 'MUX', header: true }, { data: 'HLS_JS_1_1', header: true }, { data: 'THEO', header: true }]])
+    .addTable([
+      [{ data: "Test #", header: true }, { data: 'VIDEO_JS_7', header: true }, { data: 'MUX', header: true }, { data: 'HLS_JS_1_1', header: true }, { data: 'THEO', header: true }]
+    ])
     .write();
 }
