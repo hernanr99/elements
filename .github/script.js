8f6ec3cb689b2bfd284c23c78edd0e40ca6601f1
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 14:26:38 -0300

    Update script.js

--- .github/script.js
+++ .github/script.js
@@ -2,6 +2,6 @@ module.exports = async ({core}, data) => {
   var obj = JSON.parse(data);
   await core.summary
     .addHeading("goliii", 3)
-    .addTable(obj.Reference)
+    .addTable(obj.results.Reference)
     .write();
 }
