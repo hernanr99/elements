cac9aef9f26f5d4389beaee6bc59e08b97a1e907
ghbogarin <103210250+ghbogarin@users.noreply.github.com>
2022-10-27 17:40:15 -0300

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,14 +1,14 @@
 module.exports = async ({core}, data) => {  
-  console.log(data.data);
-  const algo = {
-    results: '"[{\\"data\\":\\"Symbol\\",\\"header\\":true},{\\"data\\":\\"Description\\",\\"header\\":true}],[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]"'
-  }
-  const otro = algo.results.replaceAll('\\', '');
-  const otro2 = otro.replaceAll('[', '');
-  console.log(otro2);
+  console.log(data);
+//   const algo = {
+//     results: '"[{\\"data\\":\\"Symbol\\",\\"header\\":true},{\\"data\\":\\"Description\\",\\"header\\":true}],[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]"'
+//   }
+//   const otro = algo.results.replaceAll('\\', '');
+//   const otro2 = otro.replaceAll('[', '');
+//   console.log(otro2);
   
-  const array = otro2.split("],");
-  console.log(array);
+//   const array = otro2.split("],");
+//   console.log(array);
   await core.summary
     .addHeading(`Reference`, 3)
     .addTable([array])
