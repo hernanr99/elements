18cba45eb817c8592177e66e2668bff52b21d1e1
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 16:39:41 -0300

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -3,7 +3,7 @@ module.exports = async ({core}, data) => {
   const algo = {
     results: '"[[{\\"data\\":\\"Symbol\\",\\"header\\":true},{\\"data\\":\\"Description\\",\\"header\\":true}],[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]]"'
   }
-  const otro = algo.results.replace('\\', '');
+  const otro = algo.results.replaceAll('\\', '');
   console.log(otro);
   
   await core.summary
