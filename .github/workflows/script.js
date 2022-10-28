f44b48666e94a28780b683eab81326ea83f674dc
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 21:50:58 -0300

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,21 +1,21 @@
 module.exports = async ({core}, data) => {  
   console.log(data);
-  
-  const algo2 = data.Reference.replaceAll('"data"', 'data');
-  const algo3 = algo2.replaceAll('"header"', 'header');
-  
-  const otro2 = algo3.replaceAll('[', '');
-  const array = otro2.split("],");
-  for (i=0;i<array.length;i++) { 
-    array[i] = array[i].replaceAll("]]", "")
-    array[i] = array[i].split(",")
-  }
-  console.log(array);
-
-  await core.summary
-    .addHeading(`Reference`, 3)
-    .addTable(array)
-    .write()
+  convertToArray(data.Reference);
   
 }
 
+function convertToArray(data){
+  const otro2 = algo3.replaceAll('[', '');
+  const otro3 = algo2.replaceAll('"', '');
+  const array = otro3.split("],");
+  for (i=0;i<array.length;i++) { 
+    array[i] = array[i].replaceAll("]]", "");
+    array[i] = array[i].split(",");
+  }
+  console.log(array);
+  
+  await core.summary
+  .addHeading(`Reference`, 3)
+  .addTable(array)
+  .write()
+}
