a872a2a96f98f03b5f542b778ef3e7ffab9837c7
hernanr99 <99908389+hernanr99@users.noreply.github.com>
2022-10-27 21:15:02 -0300

    Update hernan.js

--- .github/workflows/hernan.js
+++ .github/workflows/hernan.js
@@ -1,21 +1,23 @@
 module.exports = async ({core}, data) => {  
-  console.log(data);
   const algo = {
-    results: '"[\\":heavy_check_mark:\\",\\"Test passed\\"],[\\":x:\\",\\"Test failed\\"],[\\":no_entry_sign:\\",\\"Not tested\\"]"'
+    chromeT027VIDEO_JS_6: 'true',
+    chromeT027VIDEO_JS_7: 'true',
+    chromeT027MUX: 'true',
+    chromeT027HLS_JS_1_0: 'true',
+    chromeT027HLS_JS_1_1: 'true',
+    chromeT027HLS_JS_1_2: 'true',
+    chromeT027THEO: 'true',
+    table: '{"table":[[{"data":"Test #","header":true},{"data":"VIDEO_JS_7","header":true},{"data":"MUX","header":true},{"data":"HLS_JS_1_1","header":true},{"data":"THEO","header":true}],["T027",":no_entry_sign:",":no_entry_sign:",":no_entry_sign:",":no_entry_sign:"]],"header":"Chrome","suite":"VODSpeed"}',
+    Reference: '[[{"data":"Symbol","header":true},{"data":"Description","header":true}],[":heavy_check_mark:","Test passed"],[":x:","Test failed"],[":no_entry_sign:","Not tested"]]'
   }
-  const otro = algo.results.replaceAll('\\', '');
-  const otro3 = otro.replaceAll('"', '');
-  const otro2 = otro3.replaceAll('[', '');
-  console.log(otro2);
+
   
-  const array = otro2.split("],");
-  for (i=0;i<array.length;i++) { 
-    array[i] = array[i].split(",")
-  }
-  console.log(array);
+  const algo2 = algo.Reference.replaceAll('"data"', 'data');
+  const algo3 = algo2.replaceAll('"header"', 'header');
+  console.log(algo3);
 
   await core.summary
     .addHeading(`Reference`, 3)
-    .addTable(array)
+    .addTable(algo3)
     .write()
 }
